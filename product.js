5564cded13d852006ac981f9aa95f37d36ad91ad
Shashwat69420 <100222660+Shashwat69420@users.noreply.github.com>
2024-01-03 15:34:58 +0545

    Add files via upload

---
+++ product.js
@@ -0,0 +1,17 @@
+// document.getElementById('search-btn').onclick = function(){
+//     if(document.getElementById('filter').value == "highest"){
+//         document.getElementById('ink').style.display = "none";
+//         document.getElementById('ink2').style.display = "none";
+//         document.getElementById('ink3').style.display = "none";
+//         document.getElementById('ink4').style.display = "none";
+//         document.getElementById('ink5').style.display = "none";
+//         document.getElementById('ink6').style.display = "none";
+//         document.getElementById('ink7').style.display = "none";
+//     } else if(document.getElementById('filter').value == "lowest"){
+//         document.getElementById('laser').style.display = "none";
+//         document.getElementById('laser1').style.display = "none";
+//         document.getElementById('laser2').style.display = "none";
+//         document.getElementById('laser3').style.display = "none";
+//         document.getElementById('laser4').style.display = "none";
+//     }
+// }
