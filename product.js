b9292c5938e0b19c08876503835e65c424164164
Shashwat69420 <100222660+Shashwat69420@users.noreply.github.com>
2024-01-05 13:04:30 +0545

    Update product.js

--- product.js
+++ product.js
@@ -1,1 +1,83 @@
-// nth here
+//zooming the main picture when hovering
+// document.getElementById('mainimg1').onmouseover = function(){
+//     document.getElementById('mainimg1').style.scale = "1.1";
+// }
+// //returning the picture to normal state when not hovering
+// document.getElementById('mainimg1').onmouseout = function(){
+//     document.getElementById('mainimg1').style.scale = "1";
+// }
+
+//first product hover effect
+
+document.getElementById('subimg1').onmouseover = function(){
+    document.getElementById('mainimg1').src = "images/Printer/DCP.jpg";
+}
+document.getElementById('subimg2').onmouseover = function(){
+    document.getElementById('mainimg1').src = "images/Printer/DCP2.jpg";
+}
+document.getElementById('subimg3').onmouseover = function(){
+    document.getElementById('mainimg1').src = "images/Printer/DCP3.jpg";
+}
+
+//zooming the main picture when hovering
+// document.getElementById('mainimg2').onmouseover = function(){
+//     document.getElementById('mainimg2').style.scale = "1.1";
+// }
+// //returning the picture to normal state when not hovering
+// document.getElementById('mainimg2').onmouseout = function(){
+//     document.getElementById('mainimg2').style.scale = "1";
+// }
+
+//second product hover effect
+
+document.getElementById('sub2img1').onmouseover = function(){
+    document.getElementById('mainimg2').src = "images/Printer/t820DW.jpg";
+}
+document.getElementById('sub2img2').onmouseover = function(){
+    document.getElementById('mainimg2').src = "images/Printer/t820DW2.jpg";
+}
+document.getElementById('sub2img3').onmouseover = function(){
+    document.getElementById('mainimg2').src = "images/Printer/t820DW3.jpg";
+}
+document.getElementById('sub2img4').onmouseover = function(){
+    document.getElementById('mainimg2').src = "images/Printer/t820DW4.jpg";
+}
+
+//zooming the main picture when hovering
+// document.getElementById('mainimg3').onmouseover = function(){
+//     document.getElementById('mainimg3').style.scale = "1.1";
+// }
+// //returning the picture to normal state when not hovering
+// document.getElementById('mainimg3').onmouseout = function(){
+//     document.getElementById('mainimg3').style.scale = "1";
+// }
+
+//third product hover
+
+document.getElementById('sub3img1').onmouseover = function(){
+    document.getElementById('mainimg3').src = "images/Printer/T420W.jpg";
+}
+document.getElementById('sub3img2').onmouseover = function(){
+    document.getElementById('mainimg3').src = "images/Printer/T420W2.jpg";
+}
+
+//zooming the main picture when hovering
+// document.getElementById('mainimg4').onmouseover = function(){
+//     document.getElementById('mainimg4').style.scale = "1.1";
+// }
+// //returning the picture to normal state when not hovering
+// document.getElementById('mainimg4').onmouseout = function(){
+//     document.getElementById('mainimg4').style.scale = "1";
+// }
+
+//fourth product hover
+
+document.getElementById('sub4img1').onmouseover = function(){
+    document.getElementById('mainimg4').src = "images/Printer/T720DW.jpg";
+}
+document.getElementById('sub4img2').onmouseover = function(){
+    document.getElementById('mainimg4').src = "images/Printer/T720DW2.jpg";
+}
+document.getElementById('sub4img3').onmouseover = function(){
+    document.getElementById('mainimg4').src = "images/Printer/T720DW3.jpg";
+}
